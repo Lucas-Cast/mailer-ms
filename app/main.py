aef57aed3650f0407a3db577a484624114e5c1d2
Lucas Castelano <lucas.castelano27@gmail.com>
2026-04-30 15:57:11 -0300

    config: run workers alogside the api

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
+import asyncio
+
 from fastapi import FastAPI
 from fastapi.concurrency import asynccontextmanager
 
 from app.core.db import create_tables, shutdown_engine
 from app.core.seeds import run_seeds
 from app.routers.notification import router as notifications_router
+from app.workers.consumer import main as start_consumer
 
 
 @asynccontextmanager
@@ -11,7 +14,18 @@ async def lifespan(app: FastAPI):
     await create_tables()
     await run_seeds()
 
+    ## worker is running alongside the API,
+    # so we use create_task to run it in the background
+    # otherwise the worker would need to be deployed separately, which is not the goal of this project
+    consumer_task = asyncio.create_task(start_consumer())
+
     yield
+
+    consumer_task.cancel()
+    try:
+        await consumer_task
+    except asyncio.CancelledError:
+        pass  # noqa: S7497 - Expected cancellation during shutdown
 
     await shutdown_engine()
 
